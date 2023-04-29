5fa7b5e25a351e7b3d018794011b8b03d6f81d42
1220917 <1220917@isep.ipp.pt>
2023-04-29 21:03:33 +0100

    add more some classes and repositories and some changes

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/PropertyTypeRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/PropertyTypeRepository.java
@@ -1,5 +1,11 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
+import pt.ipp.isep.dei.esoft.project.domain.Property;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class PropertyTypeRepository {
 
 
