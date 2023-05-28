a89eb742dbbdcd5dca5f3b8e89557b8bb57b2714
RodrigoCardoso1221083 <1221083@isep.ipp.pt>
2023-05-28 12:43:16 +0100

    US012 Design and Implementation Part 13

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/utils/Files.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/utils/Files.java
@@ -1,7 +1,7 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.utils;
 
 public class Files {
-    public static String path = "C:\\Users\\rodri\\Downloads\\";
+    public static String path = "src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/utils/";
 
     public static String txt = "C:\\Users\\rodri\\Downloads\\passwords.txt";
 
